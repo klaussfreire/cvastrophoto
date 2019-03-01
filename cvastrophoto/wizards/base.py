0cc4e8ee3c5cc2664459c1341c2e4fd1352a2819
Claudio Freire <klaussfreire@gmail.com>
2019-03-01 04:28:41 -0300

    Customizable brightness and gamma in get_image

--- cvastrophoto/wizards/base.py
+++ cvastrophoto/wizards/base.py
@@ -8,18 +8,18 @@ class BaseWizard:
     def _get_raw_instance(self):
         raise NotImplementedError
 
-    def get_image(self):
+    def get_image(self, bright=1.0, gamma=0.45):
         img = self._get_raw_instance()
 
         accum = self.accum
-        accum = accum.astype(numpy.float32) * (1.0 / accum.max())
+        accum = accum.astype(numpy.float32) * (bright / accum.max())
         accum = numpy.clip(accum, 0, 1, out=accum)
 
         if img.postprocessing_params.no_auto_scale:
             # Must manually gamma-encode with sRGB gamma
             accum += 0.00313
-            accum = numpy.power(accum, 0.45)
-            accum -= math.pow(0.00313, 0.45) - 0.707 / 255.0
+            accum = numpy.power(accum, gamma, out=accum)
+            accum -= math.pow(0.00313, gamma) - 0.707 / 255.0
             accum = numpy.clip(accum, 0, 1, out=accum)
 
         img.set_raw_image(accum * 65535)
