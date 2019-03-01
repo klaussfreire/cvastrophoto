baba002c21172aa4410191a6b050582c58a44ea5
Claudio Freire <klaussfreire@gmail.com>
2019-03-01 09:06:19 -0300

    Centroid tracking improvements

--- cvastrophoto/rops/tracking/centroid.py
+++ cvastrophoto/rops/tracking/centroid.py
@@ -18,17 +18,22 @@ class CentroidTrackingRop(BaseRop):
         else:
             self.reference = None
 
-    def detect(self, data):
+    def detect(self, data, hint=None):
         self.raw.set_raw_image(data)
         luma = numpy.sum(self.raw.postprocessed, axis=2, dtype=numpy.uint32)
 
         margin = min(128, min(luma.shape) / 2)
         luma = luma[margin:-margin, margin:-margin]
 
-        # Find the brightest spot to build a tracking window around it
-        pos = numpy.argmax(luma)
-        ymax = pos / luma.shape[1]
-        xmax = pos - ymax * luma.shape[1]
+        if hint is None:
+            # Find the brightest spot to build a tracking window around it
+            pos = numpy.argmax(luma)
+            ymax = pos / luma.shape[1]
+            xmax = pos - ymax * luma.shape[1]
+        else:
+            ymax, xmax = hint
+            ymax = int(ymax - margin)
+            xmax = int(xmax - margin)
 
         wleft = min(xmax, 256)
         wright = min(luma.shape[1] - xmax, 256)
@@ -49,15 +54,17 @@ class CentroidTrackingRop(BaseRop):
 
         # Translate to image space
         xoffs = xtrack - wleft + xmax + margin
-        yoffs = ytrack - wleft + ymax + margin
+        yoffs = ytrack - wup + ymax + margin
 
         return (yoffs, xoffs)
 
     def correct(self, data, bias=None, img=None):
         if bias is None:
-            bias = self.detect(data)
+            bias = self.detect(data, hint=self.reference)
         if self.reference is None:
+            # re-detect with hint, initial detection isn't so great
             self.reference = bias
+            bias = self.detect(data, hint=bias)
 
         yoffs, xoffs = bias
         yref, xref = self.reference
@@ -70,18 +77,22 @@ class CentroidTrackingRop(BaseRop):
         xdrift = int(fxdrift / pattern_shape[1]) * pattern_shape[1]
         ydrift = int(fydrift / pattern_shape[0]) * pattern_shape[0]
 
-        logger.info("Tracking offset for %s %r quantized %r",
+        logger.info("Tracking offset for %s %r quantized drift %r",
             img, (xoffs, yoffs), (xdrift, ydrift))
 
-        # move data
+        # move data - must be careful about copy direction
         if ydrift > 0:
-            if xdrift:
+            if xdrift > 0:
                 data[:ydrift-1:-1,xdrift:] = data[-ydrift-1::-1,:-xdrift]
+            elif xdrift < 0:
+                data[:ydrift-1:-1,:xdrift] = data[-ydrift-1::-1,-xdrift:]
             else:
                 data[:ydrift-1:-1] = data[-ydrift-1::-1]
         elif ydrift < 0:
-            if xdrift:
+            if xdrift > 0:
                 data[:ydrift,xdrift:] = data[-ydrift:,:-xdrift]
+            elif xdrift < 0:
+                data[:ydrift,:xdrift] = data[-ydrift:,-xdrift:]
             else:
                 data[:ydrift] = data[-ydrift:]
         elif xdrift > 0:
