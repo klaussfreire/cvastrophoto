e7e687b1bb0196e6b37550023f7c7326e2bb4b81
Claudio Freire <klaussfreire@gmail.com>
2019-04-14 14:29:59 -0300

    More configurable and faster localgradient ROP
    - Allow enabling/disabling each preprocessing step to suit other
      uses (eg: as per-frame debias rop)
    - Use multiprocessing to process each channel in parallel

--- cvastrophoto/rops/bias/localgradient.py
+++ cvastrophoto/rops/bias/localgradient.py
@@ -13,24 +13,40 @@ class LocalGradientBiasRop(BaseRop):
     gauss_size = 256
     pregauss_size = 4
     gain = 1
+    despeckle = True
 
     def detect(self, data, quick=False, **kw):
         path, patw = self._raw_pattern.shape
         local_gradient = numpy.empty(data.shape, data.dtype)
         data = self.raw.demargin(data)
-        for y in xrange(path):
-            for x in xrange(patw):
+        def compute_local_gradient(task):
+            data, local_gradient, y, x = task
+            if self.despeckle:
                 if quick:
                     despeckled = scipy.ndimage.maximum_filter(data[y::path, x::patw], 2)
                 else:
                     despeckled = skimage.filters.median(data[y::path, x::patw], skimage.morphology.disk(1))
-                local_gradient[y::path, x::patw] = scipy.ndimage.gaussian_filter(
-                    scipy.ndimage.minimum_filter(
-                        scipy.ndimage.gaussian_filter(despeckled, self.pregauss_size),
-                        self.minfilter_size
-                    ),
-                    min(8, self.gauss_size) if quick else self.gauss_size
-                ) * self.gain
+            else:
+                despeckled = data[y::path, x::patw]
+            if self.pregauss_size:
+                despeckled = scipy.ndimage.gaussian_filter(despeckled, self.pregauss_size)
+            local_gradient[y::path, x::patw] = scipy.ndimage.gaussian_filter(
+                scipy.ndimage.minimum_filter(despeckled, self.minfilter_size),
+                min(8, self.gauss_size) if quick else self.gauss_size
+            ) * self.gain
+
+        if self.raw.default_pool is not None:
+            map_ = self.raw.default_pool.map
+        else:
+            map_ = map
+
+        tasks = []
+        for y in xrange(path):
+            for x in xrange(patw):
+                tasks.append((data, local_gradient, y, x))
+        for _ in map_(compute_local_gradient, tasks):
+            pass
+
         return local_gradient
 
     def correct(self, data, local_gradient=None, quick=False, **kw):
