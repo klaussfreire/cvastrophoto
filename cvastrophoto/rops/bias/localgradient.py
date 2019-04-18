18282f416d64c38203c8183c8c24673a207005ef
Claudio Freire <klaussfreire@gmail.com>
2019-04-18 11:47:20 -0300

    Offset option for localgradient
    Helps preserve low-intensity detail, crucial when using as per-frame
    debias rop

--- cvastrophoto/rops/bias/localgradient.py
+++ cvastrophoto/rops/bias/localgradient.py
@@ -13,6 +13,7 @@ class LocalGradientBiasRop(BaseRop):
     gauss_size = 256
     pregauss_size = 4
     gain = 1
+    offset = -1
     despeckle = True
 
     def detect(self, data, quick=False, **kw):
@@ -34,6 +35,18 @@ class LocalGradientBiasRop(BaseRop):
                 scipy.ndimage.minimum_filter(despeckled, self.minfilter_size),
                 min(8, self.gauss_size) if quick else self.gauss_size
             ) * self.gain
+
+            offset = self.offset
+            if local_gradient.dtype.kind not in ('i', 'u'):
+                local_gradient[y::path, x::patw] += offset
+            elif offset < 0:
+                iinfo = numpy.iinfo(local_gradient.dtype)
+                local_gradient[y::path, x::patw][local_gradient[y::path, x::patw] <= iinfo.min-offset] = 0
+                local_gradient[y::path, x::patw][local_gradient[y::path, x::patw] > iinfo.min-offset] -= -offset
+            elif offset > 0:
+                iinfo = numpy.iinfo(local_gradient.dtype)
+                local_gradient[y::path, x::patw][local_gradient[y::path, x::patw] < iinfo.max-offset] += offset
+                local_gradient[y::path, x::patw][local_gradient[y::path, x::patw] >= iinfo.max-offset] = iinfo.max
 
         if self.raw.default_pool is not None:
             map_ = self.raw.default_pool.map
