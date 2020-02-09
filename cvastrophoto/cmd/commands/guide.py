be7a5f26bc1ea4aa67a51a3865dc79787ec4ad52
Claudio Freire <klaussfreire@gmail.com>
2020-02-09 15:00:47 -0300

    Logging

--- cvastrophoto/cmd/commands/guide.py
+++ cvastrophoto/cmd/commands/guide.py
@@ -468,7 +468,9 @@ possible to give explicit per-component units, as:
         elif from_ and speed:
             to_gc = self.parse_coord(to_)
             from_gc = self.parse_coord(from_)
+            logging.info("Shifting from %s to %s", from_gc, to_gc)
             ra_off, dec_off = from_gc.spherical_offsets_to(to_gc.transform_to(from_gc))
+            logging.info("Shifting will take %s RA %s DEC", ra_off.hms, dec_off.dms)
             self.guider.shift(dec_off.arcsec, ra_off.arcsec, speed)
         else:
             logger.error("Without a mount connected, from and speed are mandatory")
