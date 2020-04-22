c885d2fc5b8f83e3c64fdbc7534832d3c63a76dc
Claudio Freire <klaussfreire@gmail.com>
2020-04-22 04:50:01 -0300

    Improve selection wizard
    Add option for a cleanup ROP, by default a median filter, to
    get rid of bad pixels and other imperfections that can be mistaken
    for sharp desirable features.

--- cvastrophoto/wizards/selection.py
+++ cvastrophoto/wizards/selection.py
@@ -8,6 +8,7 @@ import operator
 
 from .base import BaseWizard
 from ..rops.measures import focus
+from ..rops.denoise import median
 from ..library import darks
 
 import logging
@@ -19,6 +20,8 @@ class SubSelectionWizard(BaseWizard):
     def __init__(self,
             selection_class=focus.FocusMeasureRop,
             selection_kwargs=dict(quick=True),
+            cleanup_class=median.MedianFilterRop,
+            cleanup_kwargs={},
             best_ratio=0.7,
             pool=None):
 
@@ -27,6 +30,8 @@ class SubSelectionWizard(BaseWizard):
 
         self.selection_class = selection_class
         self.selection_kwargs = selection_kwargs
+        self.cleanup_class = cleanup_class
+        self.cleanup_kwargs = cleanup_kwargs
         self.best_ratio = best_ratio
         self.dark_library = None
 
@@ -36,16 +41,23 @@ class SubSelectionWizard(BaseWizard):
         if dark_library is None:
             dark_library = darks.DarkLibrary(default_pool=self.pool)
 
+        sub = next(subs)
         self.dark_library = dark_library
-        self.selection = self.selection_class(next(subs), **self.selection_kwargs)
+        self.selection = self.selection_class(sub, **self.selection_kwargs)
 
-    def select(self, subs, nsubs=None):
+        if self.cleanup_class is not None:
+            self.cleanup = self.cleanup_class(sub, **self.cleanup_kwargs)
+        else:
+            self.cleanup = None
+
+    def _rank_subs(self, subs, nsubs=None):
         if self.pool is not None:
             imap = self.pool.imap_unordered
         else:
             imap = itertools.imap
 
         selection = self.selection
+        cleanup = self.cleanup
         dark_library = self.dark_library
 
         def rank(item):
@@ -59,7 +71,12 @@ class SubSelectionWizard(BaseWizard):
                     if dark is not None:
                         sub.denoise([dark], quick=True, master_bias=None, entropy_weighted=False)
 
-                rank = selection.measure_scalar(sub.rimg.raw_image)
+                raw_image = sub.rimg.raw_image
+
+                if cleanup is not None:
+                    raw_image = cleanup.correct(raw_image, pool=None)
+
+                rank = selection.measure_scalar(raw_image)
                 sub.close()
                 return i, sub, rank
             except Exception:
@@ -74,14 +91,26 @@ class SubSelectionWizard(BaseWizard):
             nsubs = '?'
 
         last_status = time.time()
-        ranked = []
+        ranked = 0
         for i, sub, sub_rank in imap(rank, enumerate(subs)):
-            ranked.append((sub_rank, i, sub))
+            yield i, sub, sub_rank
+            ranked += 1
             if time.time() > last_status + 10:
-                logger.info("Ranked %d/%s subs", len(ranked), nsubs)
+                logger.info("Ranked %d/%s subs", ranked, nsubs)
                 last_status = time.time()
 
-        logger.info("Ranked %d/%s subs", len(ranked), nsubs)
+        logger.info("Ranked %d/%s subs", ranked, nsubs)
+
+    def rank(self, subs, nsubs=None):
+        ranked = []
+        for i, sub, sub_rank in self._rank_subs(subs, nsubs=nsubs):
+            ranked.append((sub_rank, i, sub))
+
+        ranked.sort(key=operator.itemgetter(0))
+        return ranked
+
+    def select(self, subs, nsubs=None):
+        ranked = self.rank(subs, nsubs=nsubs)
 
         ranked.sort(key=operator.itemgetter(0))
         nselected = int(max(1, len(ranked) * self.best_ratio))
