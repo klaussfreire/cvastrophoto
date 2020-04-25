626c6de01354ed4a8f0841d88b9da6e11ce830ea
Claudio Freire <klaussfreire@gmail.com>
2020-04-25 11:23:10 -0300

    Focus: clamp to minimum analysis size

--- cvastrophoto/rops/measures/focus.py
+++ cvastrophoto/rops/measures/focus.py
@@ -17,7 +17,7 @@ class FocusMeasureRop(base.PerChannelMeasureRop):
     measure_dtype = numpy.float32
 
     def measure_channel(self, channel_data, detected=None, channel=None):
-        isize = int(max(channel_data.shape) * self.size)
+        isize = max(int(max(channel_data.shape) * self.size), 4)
         edge = scipy.ndimage.sobel(channel_data.astype(numpy.float32, copy=False))
         edge *= 1.0 / 65535
         edge = (
