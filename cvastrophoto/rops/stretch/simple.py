33c0c43561e0dde0777556a16f70153391e5dd38
Claudio Freire <klaussfreire@gmail.com>
2020-08-02 21:18:49 -0300

    Add simple stretching ROP

---
+++ cvastrophoto/rops/stretch/simple.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+import logging
+import numpy
+
+from .. import base
+
+logger = logging.getLogger(__name__)
+
+class LinearStretchRop(base.BaseRop):
+
+    bright = 4.0
+
+    def correct(self, data, detected=None, **kw):
+        dmax = data.max()
+        if data.dtype.kind in 'fd':
+            data *= self.bright
+            data = numpy.clip(data, 0, dmax, out=data)
+        else:
+            data = numpy.clip(data * self.bright, 0, dmax, out=data)
+
+        return data
