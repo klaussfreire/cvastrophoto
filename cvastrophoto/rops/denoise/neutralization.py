7c178f451e35dd8c2741638870c59e76b93c2bac
Claudio Freire <klaussfreire@gmail.com>
2020-08-15 16:46:01 -0300

    Additive neutralization

--- cvastrophoto/rops/denoise/neutralization.py
+++ cvastrophoto/rops/denoise/neutralization.py
@@ -10,12 +10,14 @@ class BackgroundNeutralizationRop(BaseRop):
 
     scale = 128
     sigma = 4.0
+    mode = 'add'
 
     def __init__(self, raw, **kw):
         kw.setdefault('despeckle', True)
         kw.setdefault('despeckle_size', 3)
         kw.setdefault('pregauss_size', 3)
         kw.setdefault('aggressive', True)
+        kw.setdefault('close_factor', 0)
         self.scale = scale = int(kw.setdefault('scale', self.scale))
         remove_stars_kw = {k: kw.pop(k) for k in list(kw) if hasattr(BackgroundRemovalRop, k)}
         remove_stars_kw.setdefault('minfilter_size', scale)
@@ -37,16 +39,26 @@ class BackgroundNeutralizationRop(BaseRop):
         self.parallel_channel_task(bg, bg, flatten)
 
         bgmax = bg.max()
+        bgavg = numpy.average(bg)
         bgstd = numpy.std(bg)
-        if bgmax > 0.1:
-            bg *= 1.0 / bgmax
-        bgmin = bg.min()
-        if bgmin < 0.1:
-            bgmin = numpy.percentile(bg, 25)
-            bg = numpy.clip(bg, bgmin, None, out=bg)
 
-        fdata = data.astype(numpy.float32, copy=False)
-        ndata = data.astype(numpy.float32, copy=False) / bg
-        ndata = numpy.clip(ndata, None, dmax, out=ndata)
-        data[:] = (ndata * (self.sigma * bgstd) + data * fdata) / (self.sigma * bgstd + fdata)
+        if self.mode == 'mult':
+            if bgmax > 0.1:
+                bg *= 1.0 / bgmax
+            bgmin = bg.min()
+            if bgmin < 0.1:
+                bgmin = numpy.percentile(bg, 25)
+                bg = numpy.clip(bg, bgmin, None, out=bg)
+
+            fdata = data.astype(numpy.float32, copy=False)
+            ndata = data.astype(numpy.float32, copy=False) / bg
+            ndata = numpy.clip(ndata, None, dmax, out=ndata)
+            data[:] = (ndata * (self.sigma * (bgavg + bgstd)) + data * fdata) / (self.sigma * (bgavg + bgstd) + fdata)
+        elif self.mode == 'add':
+            bgthr = bgavg + bgstd * self.sigma
+            bgthr = numpy.average(bg[bg < bgthr])
+            mask = bg <= bgthr
+            data[mask] = numpy.clip(((data + (bgthr - bg))[mask]), None, dmax).astype(data.dtype, copy=False)
+        else:
+            raise NotImplementedError("Unimplemented neutralization mode %s" % (self.mode,))
         return data
