dd40fcb7b4512480a84dd3ea2a56f8da251d2dba
Claudio Freire <claudiofreire@localhost.localdomain>
2020-08-30 22:27:12 -0300

    Fast gaussian filter doesn't work with odd sized images

--- cvastrophoto/util/gaussian.py
+++ cvastrophoto/util/gaussian.py
@@ -30,7 +30,7 @@ def fast_gaussian(img, sigma, mode='reflect', **kw):
                     pad_kw = {'constant_values': (kw.pop('cval', 0.0),)}
 
             padded_size = (2*padding+img.shape[0]) * (2*padding+img.shape[1])
-            if padding < max(img.shape) and padded_size <= img.size * MAX_MEMORY_OVERHEAD:
+            if padding < max(img.shape) and padded_size <= img.size * MAX_MEMORY_OVERHEAD and (img.shape[-1] % 2) == 0:
                 # Avoid excessive memory overhead, the slow implementation at least doesn't use extra RAM
                 if padding:
                     padded = skimage.util.pad(img, padding, skmode, **pad_kw)
