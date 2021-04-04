4094e4d3b53aa23a44e879ed0b6ce91d2b083da0
Claudio Freire <claudiofreire@localhost.localdomain>
2021-04-04 18:06:35 -0300

    Fix white thresholding in local entropy

--- cvastrophoto/util/entropy.py
+++ cvastrophoto/util/entropy.py
@@ -9,7 +9,7 @@ def local_entropy(gray, gamma=2.4, selem=None, size=32, copy=True, white=1.0):
     if selem is None:
         selem = skimage.morphology.disk(size)
     gray = gray.astype(numpy.float32, copy=copy)
-    gray *= 1.0 / (white * 65535)
+    gray *= float(white) / 65535
     gray = numpy.clip(gray, 0, 1, out=gray)
     gray = srgb.encode_srgb(gray, gamma=gamma)
     gray *= 65535
