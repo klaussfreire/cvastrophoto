c7a00af38d4c1ec707364472cba693de87953c55
Claudio Freire <claudiofreire@linux.local>
2021-05-20 05:21:11 -0300

    Faster local_entropy
    - Unify a few operations
    - Use a lut-able encode_srgb call

--- cvastrophoto/util/entropy.py
+++ cvastrophoto/util/entropy.py
@@ -9,13 +9,11 @@ def local_entropy(gray, gamma=2.4, selem=None, size=32, copy=True, white=1.0):
     if selem is None:
         selem = skimage.morphology.disk(size)
     gray = gray.astype(numpy.float32, copy=copy)
-    gray *= 1.0 / (white * 65535)
-    gray = numpy.clip(gray, 0, 1, out=gray)
-    gray = srgb.encode_srgb(gray, gamma=gamma)
-    gray *= 65535
+    gray *= 65535.0 / (white * 65535)
+    gray = numpy.clip(gray, 0, 65535, out=gray)
     gray = gray.astype(numpy.uint16)
+    gray = srgb.encode_srgb(gray, gamma=gamma)
     gray = numpy.right_shift(gray, 8, out=gray)
-    gray = numpy.clip(gray, 0, 255, out=gray)
     gray = gray.astype(numpy.uint8)
     ent = skimage.filters.rank.entropy(gray, selem).astype(numpy.float32)
     return ent
