51a05e2a14b2d5f7f392430ab237bff26cb0ccf0
Claudio Freire <claudiofreire@linux.local>
2021-07-18 20:14:53 -0300

    Fix config loading with missing sections

--- cvastrophoto/guiding/config.py
+++ cvastrophoto/guiding/config.py
@@ -12,6 +12,9 @@ class ConfigHelperMixin(object):
     PARAM_TYPES = (bool, int, float)
 
     def load_config(self, config_file, section):
+        if not config_file.has_section(section):
+            return
+
         for opt in config_file.options(section):
             if not opt.startswith('_') and hasattr(self, opt) and isinstance(getattr(self, opt), self.param_types):
                 value = ast.literal_eval(config_file.get(section, opt))
