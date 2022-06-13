05fd51586b2e9aad6a6ea0c3d3359a39847df0b8
Claudio Freire <claudiofreire@localhost.localdomain>
2022-06-13 01:37:13 -0300

    starnet++ integration

--- cvastrophoto/rops/tracking/extraction.py
+++ cvastrophoto/rops/tracking/extraction.py
@@ -3,11 +3,21 @@ from __future__ import absolute_import
 
 import numpy
 import scipy.ndimage
+import os
+import subprocess
+import imageio
+import tempfile
+import logging
 
 from ..denoise import median
 from ..compound import CompoundRop
 from ..base import PerChannelRop
 from ..bias import localgradient
+from cvastrophoto.util import demosaic
+from cvastrophoto.image import Image
+
+
+logger = logging.getLogger(__name__)
 
 
 class BackgroundRemovalRop(localgradient.LocalGradientBiasRop):
@@ -23,15 +33,74 @@ class BackgroundRemovalRop(localgradient.LocalGradientBiasRop):
     despeckle = False
     aggressive = False
     protect_white = False
-    gain = 1
+    gain = 1.0
     offset = 0
+
+
+class StarnetStarRemovalRop(localgradient.LocalGradientBiasRop):
+
+    starnet_path = os.environ.get('STARNET_PATH', '')
+    starnet_bin = os.environ.get('STARNET_BIN', './starnet++')
+
+    def correct(self, data, detected=None, img=None):
+        starnet_dir = self.starnet_path
+        if not starnet_dir:
+            starnet_dir = '.'
+
+        cwd = os.getcwd()
+        infile = tempfile.mktemp(prefix='.cvap-starnet-', suffix='-in.tiff')
+        outfile = tempfile.mktemp(prefix='.cvap-starnet-', suffix='-out.tiff')
+
+        ppdata = data.copy()
+        if ppdata.dtype.char in 'lLiI':
+            dmax = ppdata.max()
+            if dmax <= 65535:
+                ppdata = ppdata.astype('H')
+            else:
+                ppdata = ppdata.astype('f')
+        if ppdata.dtype.kind == 'f':
+            dmax = ppdata.max()
+            scale = dmax
+            ppdata *= 65535.0 / scale
+            ppdata = numpy.clip(ppdata, 0, 65535, out=ppdata)
+            ppdata = ppdata.astype('H')
+        else:
+            scale = None
+        try:
+            logger.info("Invoking starnet from %r", starnet_dir)
+            self.raw.set_raw_image(ppdata)
+            self.raw.save(infile, meta={})
+            os.chdir(starnet_dir)
+            if subprocess.check_call([self.starnet_bin, infile, outfile]):
+                raise RuntimeError("Error invoking starnet")
+            ppdata = Image.open(outfile, autoscale=False).rimg.raw_image
+            if scale is not None:
+                ppdata = ppdata.astype('f')
+                ppdata *= scale / 65535.0
+            data[:] = ppdata
+        finally:
+            if os.path.exists(infile):
+                os.unlink(infile)
+            if os.path.exists(outfile):
+                os.unlink(outfile)
+            os.chdir(cwd)
+
+        return data
+
+
+class StarnetBackgroundRemovalRop(StarnetStarRemovalRop):
+
+    def correct(self, data, detected=None, **kw):
+        bg = super(StarnetBackgroundRemovalRop, self).correct(data.copy(), **kw)
+        data -= numpy.minimum(bg, data)
+        return data
 
 
 class WhiteTophatFilterRop(PerChannelRop):
 
     size = 1
 
-    def process_channel(self, data, detected=None):
+    def process_channel(self, data, detected=None, img=None):
         return scipy.ndimage.white_tophat(data, self.size)
 
 
@@ -45,16 +114,29 @@ class ExtractStarsRop(CompoundRop):
     close_factor = 1.0
     offset = 0
 
+    method = 'localgradient'
+
+    starnet_path = StarnetStarRemovalRop.starnet_path
+    starnet_bin = StarnetStarRemovalRop.starnet_bin
+
     def __init__(self, raw, **kw):
         self.median_size = median_size = int(kw.pop('median_size', self.median_size))
         self.median_sigma = median_sigma = float(kw.pop('median_sigma', self.median_sigma))
         self.star_size = star_size = int(kw.pop('star_size', self.star_size))
         self.offset = offset = int(kw.pop('offset', self.offset))
+        self.method = method = kw.pop('method', self.method)
 
         if self.quick:
             extract_rop = WhiteTophatFilterRop(raw, size=star_size, **kw)
         else:
-            extract_rop = BackgroundRemovalRop(raw, minfilter_size=star_size, gauss_size=star_size, offset=offset, **kw)
+            if method == 'localgradient':
+                skw = kw.copy()
+                skw.setdefault('minfilter_size', star_size)
+                skw.setdefault('gauss_size', star_size)
+                skw.setdefault('star_size', star_size)
+                extract_rop = BackgroundRemovalRop(raw, offset=offset, **skw)
+            else:
+                extract_rop = StarnetBackgroundRemovalRop(raw, **kw)
 
         super(ExtractStarsRop, self).__init__(
             raw,
