9a0e4b3245fa9d6820e33081c91085e6995adb58
Claudio Freire <klaussfreire@gmail.com>
2022-08-04 01:37:52 -0300

    Try to improve post-dither stabilization
    - Avoid false positive stability measurements after force-stop dither
    - Clear stable history at dither time to avoid taking pre-dither stability
      samples into account

--- cvastrophoto/cmd/commands/guide.py
+++ cvastrophoto/cmd/commands/guide.py
@@ -884,6 +884,7 @@ class CaptureSequence(object):
 
                     self.state_detail = 'wait stable'
                     self.sleep(self.stabilization_s)
+                    self.guider.stable_history.clear()
                     self.guider.wait_stable(self.stabilization_px, self.stabilization_s, stabilization_s_max)
                     next_dither = self.dither_interval
 
@@ -892,7 +893,9 @@ class CaptureSequence(object):
                         logger.info("Force-stop dither")
                         self.state_detail = 'force stop'
                         self.guider.stable_history.clear()
-                        self.guider.wait_stable(self.stabilization_px, self.stabilization_s, stabilization_s_max)
+                        self.guider.wait_stable(
+                            self.stabilization_px, self.stabilization_s, stabilization_s_max,
+                            stable_pulses=3) # After force stop, at least 1 pulse will be artificially stable
                     if self.guider.state != 'guiding':
                         logger.info("Not stabilized, continuing anyway")
                     else:
