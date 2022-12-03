fa65dd495b0b5329801fa0f1b15da01027bc7673
Claudio Freire <claudiofreire@localhost.localdomain>
2022-12-03 20:24:14 -0300

    Support for coorindate maps in accel.skimage.transform

--- cvastrophoto/accel/skimage/transform.py
+++ cvastrophoto/accel/skimage/transform.py
@@ -57,7 +57,7 @@ if with_cupy:
             mode='constant', cval=0., clip=True, preserve_range=False, output_shape=None,
             **kw):
 
-        matrix = None
+        matrix = coords = None
 
         if kw is None and image.size and image.ndim == 2 or order in (0, 1, 2, 3, 4, 5):
 
@@ -71,42 +71,58 @@ if with_cupy:
                 # inverse_map is a homography
                 matrix = inverse_map.params
 
-        if matrix is None:
-            return sk_warp(
-                image, inverse_map,
-                order=order, mode=mode, cval=cval, clip=clip, preserve_range=preserve_range,
-                output_shape=output_shape,
-                **kw)
+            elif isinstance(inverse_map, np.ndarray):
+                # inverse_map is directly given as coordinates
+                coords = inverse_map
 
-        if matrix.shape[0] == matrix.shape[1]:
-            # Apply coordinate transform since ndimage uses y,x instead of x,y
-            S = np.identity(matrix.shape[0])
-            S[[0,1]] = S[[1,0]]
+        if matrix is not None:
+            def _warp(image, matrix):
+                if matrix.shape[0] == matrix.shape[1]:
+                    # Apply coordinate transform since ndimage uses y,x instead of x,y
+                    S = np.identity(matrix.shape[0])
+                    S[[0,1]] = S[[1,0]]
 
-            matrix = np.matmul(S, np.matmul(matrix, S))
+                matrix = cp.asanyarray(matrix)
+                image = cp.asanyarray(image)
+                warped = ndi.affine_transform(
+                    image, matrix,
+                    output_shape=output_shape, order=order, mode=mode, cval=cval)
 
-        def _warp(image, matrix):
-            matrix = cp.asanyarray(matrix)
-            image = cp.asanyarray(image)
-            warped = ndi.affine_transform(
-                image, matrix,
-                output_shape=output_shape, order=order, mode=mode, cval=cval)
+                if clip:
+                    _clip_warp_output(image, warped, mode, cval)
 
-            if clip:
-                _clip_warp_output(image, warped, mode, cval)
+                return cp.asnumpy(warped)
+            args = (image, matrix)
+            req_mem = image.size * image.dtype.itemsize * 2
+        elif coords is not None and output_shape is None:
+            def _warp(image, coords):
+                coords = cp.asanyarray(coords)
+                image = cp.asanyarray(image)
+                warped = ndi.map_coordinates(
+                    image, coords,
+                    order=order, mode=mode, cval=cval)
 
-            return cp.asnumpy(warped)
+                if clip:
+                    _clip_warp_output(image, warped, mode, cval)
 
-        try:
-            return in_cuda_pool(image.size * image.dtype.itemsize * 2, _warp, image, matrix).get()
-        except (MemoryError, cupy.cuda.memory.OutOfMemoryError):
-            logger.warning("Out of memory during CUDA transform, falling back to CPU")
-            cupy_oom_cleanup()
-            return sk_warp(
-                image, inverse_map,
-                order=order, mode=mode, cval=cval, clip=clip, preserve_range=preserve_range,
-                output_shape=output_shape,
-                **kw)
+                return cp.asnumpy(warped)
+            args = (image, coords)
+            req_mem = image.size * image.dtype.itemsize * 2 + coords.size * coords.dtype.itemsize
+        else:
+            _warp = None
+
+        if _warp is not None:
+            try:
+                return in_cuda_pool(req_mem, _warp, *args).get()
+            except (MemoryError, cupy.cuda.memory.OutOfMemoryError):
+                logger.warning("Out of memory during CUDA transform, falling back to CPU")
+                cupy_oom_cleanup()
+
+        return sk_warp(
+            image, inverse_map,
+            order=order, mode=mode, cval=cval, clip=clip, preserve_range=preserve_range,
+            output_shape=output_shape,
+            **kw)
 
 else:
     warp = sk_warp
